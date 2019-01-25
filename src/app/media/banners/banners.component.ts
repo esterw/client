60ac18865d39729dc63120862ed6c416cc705b77
esterw <wwr1109@gmail.com>
2019-01-25 13:25:46 +0200

    sdf

--- src/app/media/banners/banners.component.ts
+++ src/app/media/banners/banners.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, ChangeDetectorRef, OnChanges, AfterViewChecked } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { AffiliateService } from '../../shared/affiliate-server/affiliate.service';
 import { AffiliateBanner, AffiliatesBanners } from '../../shared/affiliate-server/affiliate.model';
 import * as FileSaver from 'file-saver';
 import { TooltipModule } from "ng2-tooltip";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-banners',
@@ -18,7 +19,8 @@ export class BannersComponent implements OnInit, AfterViewChecked {
   affiliateID: number;
 
   constructor(private service: AffiliateService,
-    private cdr: ChangeDetectorRef) { }
+    private cdr: ChangeDetectorRef,
+    private http: HttpClient) { }
 
   ngOnInit() {
     this.subscription = this.service.affiliateChanged.subscribe(affiliate => {
@@ -40,16 +42,32 @@ export class BannersComponent implements OnInit, AfterViewChecked {
   }
 
   downloadBanner(bannerName: string, bannerID: number) {
-    fetch('http://localhost:50741/bannerh.ashx?Banner=' + bannerID).
-      then(function (response) {
+    // fetch('http://localhost:50741/bannerh.ashx?Banner=' + bannerID).
+    //   then(function (response) {
 
-        return response.blob();
-      }).then(function (myBlob) {
-        FileSaver.saveAs(myBlob, bannerName + myBlob.type);
-      })
+    //     return response.blob();
+    //   }).then(function (myBlob) {
+    //     FileSaver.saveAs(myBlob, bannerName + myBlob.type);
+    //   })
+    this.getBanner(bannerID, bannerName)
   }
 
   changeToFalse() {
 
   }
+
+  getBanner(bannerID, bannerName) {
+    this.getFormBanner(bannerID).subscribe(myBlob => {
+      FileSaver.saveAs(myBlob, bannerName + myBlob.type);
+    });
+  }
+
+  getFormBanner(bannerID): Observable<any> {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:50741/bannerh.ashx?Banner=' + bannerID).map(
+        (res: Response) => {
+            return res.blob();
+        });
+}
 }
